ed74e4b4f6c8452216e22ee71d1e5b8e0b2a1ae0
Diana Malinina <malinina.svlk@gmail.com>
2022-02-21 15:38:38 +0200

    add new projects

---
+++ ShoppingList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myShoppingList = new List<string>();
+
+            Console.WriteLine(myShoppingList.Count);
+
+            myShoppingList.Add("milk");
+            myShoppingList.Add("potato");
+            myShoppingList.Add("apple");
+            myShoppingList.Add("coffee");
+
+            Console.WriteLine(myShoppingList.Count);
+
+
+            foreach (string wish in myShoppingList)
+            {
+                Console.WriteLine(wish);
+            }
+        }
+    }
+}
